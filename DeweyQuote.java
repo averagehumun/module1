0bd766d19806c48166b0ceb75a84220044f98af1
average humun <113070026+averagehumun@users.noreply.github.com>
2024-03-25 10:49:21 -0600

    Add files via upload

---
+++ DeweyQuote.java
@@ -0,0 +1,18 @@
+// *********************************************************************
+// DeweyQuote.java           Authhor: Dylan
+//
+// Used to review the basic program elements of a Java program.
+// *********************************************************************
+
+public class DeweyQuote {
+
+    public static void main(String[] args) {
+        //--------------------------------------------------------------
+        // Prints a quote from John Dewey.
+        //--------------------------------------------------------------
+        System.out.println("A problem well-stated is half solved."); 
+        System.out.println("-- John Dewey.");
+
+    }
+
+}
